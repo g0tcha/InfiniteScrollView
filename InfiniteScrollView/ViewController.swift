7762daee60492cda9b88ded0f5d3051e8b7af6fe
g0tcha <vincent.grossier@gmail.com>
2018-12-03 19:25:17 +0100

    Initial Commit

---
+++ InfiniteScrollView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InfiniteScrollView
+//
+//  Created by Vincent on 03/12/2018.
+//  Copyright © 2018 Kodappy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
